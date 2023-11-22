d42b1197382b1b12d62d4873df3f22f70ac31232
Samandar1418 <nabiyevs967@gmail.com>
2023-11-22 20:58:23 +0500

    finish

--- js/api.js
+++ js/api.js
@@ -1,10 +1,9 @@
-const search = document.getElementById('search');
-const main = document.querySelector('main'); // main elementini querySelector orqali tanlangan qilib olamiz
+const searchInput = document.getElementById('search');
+const main = document.querySelector('main'); 
 
 document.addEventListener('DOMContentLoaded', function () {
     fetch('https://jsonplaceholder.typicode.com/comments')
         .then((response) => {
-          
             return response.json();
         })
         .then((dataJson) => {
@@ -17,10 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function container(dataJson) {
-    let add = ''; 
+    let add = '';
 
     dataJson.forEach((data) => {
-        
         let card = `
             <div class="div">
                 <p>ID: ${data.id}</p>
@@ -30,5 +28,29 @@ function container(dataJson) {
         `;
         add += card;
     });
-    main.innerHTML = add; 
+    main.innerHTML = add;
+
+    searchInput.addEventListener('input', function () {
+        let inputValue = this.value;
+        let filteredData = dataJson.filter(el => {
+            return el.name.includes(inputValue) || el.email.includes(inputValue);
+        });
+        updateContainer(filteredData);
+    });
+}
+
+function updateContainer(data) {
+    let add = '';
+
+    data.forEach((item) => {
+        let card = `
+            <div class="div">
+                <p>ID: ${item.id}</p>
+                <p>Name: ${item.name}</p>
+                <p>Email: ${item.email}</p>
+            </div>
+        `;
+        add += card;
+    });
+    main.innerHTML = add;
 }
